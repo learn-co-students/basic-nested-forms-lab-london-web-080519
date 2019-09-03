7d0d947c4dcc42e596858a519c1f9d2bbdd151db
github username <aude53@gmail.com>
2019-09-03 11:07:03 +0100

    Done.

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,3 +1,9 @@
 class Ingredient < ActiveRecord::Base
   belongs_to :recipe
+
+  def ingredients_attributes=(ingredient)
+    self.ingredient = Recipe.find_or_create_by(name: ingredient[:name], quantity: quantity[:quantity])
+    self.ingredient.update(ingredient)
+  end
+
 end
