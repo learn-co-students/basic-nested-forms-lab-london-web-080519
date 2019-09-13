308cddc3f88d2c77434896df72eb6d2b109c8df3
99chris99 <chrishodge87@gmail.com>
2019-09-13 09:22:54 +0100

    Done.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -10,29 +10,19 @@ class RecipesController < ApplicationController
   def new
     @recipe = Recipe.new
     2.times {@recipe.ingredients.build}
-    #@recipe.ingredients.build
   end
-
-  # @person = Person.new
-  # @person.addresses.build(address_type: 'work')
-  # @person.addresses.build(address_type: 'home')
-
 
   def create
     @recipe = Recipe.create(recipe_params)
-    
-    #@recipe = Recipe.create(params)
-    
   end
-
 
 private
 
 def recipe_params
-  #params.require(:parent).permit(:any_parent_attributes_go_here kids_attributes: [:id, :name, :_destroy]) # This permits the kids params to be saved
   params.require(:recipe).permit(:title, ingredients_attributes:[:name, :quantity])
 end
 
+end
 # def person_params
 #   params.require(:person).permit(
 #     :name,
@@ -47,4 +37,3 @@ end
 #   )
 # end
 
-end
